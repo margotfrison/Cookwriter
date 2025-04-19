63282c57ed697ea630b0f2f6effc216335ffcaf3
Margot Frison <frison.margot@laposte.net>
2025-04-19 20:27:56 +0200

    feat: #13 - Added steps in recipe writer activity

--- app/src/main/java/me/margotfrison/cookwriter/android/activities/RecipeWriteActivity.java
+++ app/src/main/java/me/margotfrison/cookwriter/android/activities/RecipeWriteActivity.java
@@ -4,19 +4,29 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import io.vavr.control.Try;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import me.margotfrison.cookwriter.R;
 import me.margotfrison.cookwriter.android.App;
 import me.margotfrison.cookwriter.android.components.EditTextScrollable;
+import me.margotfrison.cookwriter.android.components.StepComponent;
 import me.margotfrison.cookwriter.dao.DaoUtil;
 import me.margotfrison.cookwriter.dao.DurationConverter;
 import me.margotfrison.cookwriter.dao.RecipeRepository;
@@ -24,6 +34,7 @@ import me.margotfrison.cookwriter.dto.Difficulty;
 import me.margotfrison.cookwriter.dto.Price;
 import me.margotfrison.cookwriter.dto.Recipe;
 import me.margotfrison.cookwriter.dto.Season;
+import me.margotfrison.cookwriter.dto.Step;
 
 public class RecipeWriteActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
     private EditText cleanName;
@@ -34,9 +45,14 @@ public class RecipeWriteActivity extends AppCompatActivity implements View.OnCli
     private Spinner season;
     private Spinner difficulty;
     private Spinner price;
+    private LinearLayout steps;
+    private Button addStep;
     private TextView error;
     private Button save;
     private RecipeRepository recipeRepository;
+
+    private final List<StepComponent> stepList = new ArrayList<>();
+    private boolean disableTextWatch = false;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -56,6 +72,8 @@ public class RecipeWriteActivity extends AppCompatActivity implements View.OnCli
         season = findViewById(R.id.season);
         difficulty = findViewById(R.id.difficulty);
         price = findViewById(R.id.price);
+        steps = findViewById(R.id.steps);
+        addStep = findViewById(R.id.add_step);
         error = findViewById(R.id.error_text);
         save = findViewById(R.id.save);
         recipeRepository = App.getDatabase().recipeRepository();
@@ -68,9 +86,11 @@ public class RecipeWriteActivity extends AppCompatActivity implements View.OnCli
         season.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Season.values()));
         difficulty.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Difficulty.values()));
         price.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Price.values()));
+        addStep.setOnClickListener(this);
         save.setOnClickListener(this);
 
         if (recipePreviewAuthor != null && recipePreviewCustomName != null) {
+            disableTextWatch = true;
             DaoUtil.subscribeUIThread(
                     recipeRepository.get(recipePreviewAuthor, recipePreviewCustomName),
                     (recipe) -> {
@@ -82,6 +102,8 @@ public class RecipeWriteActivity extends AppCompatActivity implements View.OnCli
                         season.setSelection(recipe.getSeason().spinnerIndex);
                         difficulty.setSelection(recipe.getDifficulty().spinnerIndex);
                         price.setSelection(recipe.getPrice().spinnerIndex);
+                        Optional.ofNullable(recipe.getSteps()).orElse(new ArrayList<>()).forEach(this::addStep);
+                        disableTextWatch = false;
                     },
                     this);
         }
@@ -94,6 +116,29 @@ public class RecipeWriteActivity extends AppCompatActivity implements View.OnCli
     protected void onDestroy() {
         super.onDestroy();
         DaoUtil.disposeAll(this);
+    }
+
+    private void addStep() {
+        addStep(null);
+    }
+
+    private void addStep(@Nullable Step step) {
+        final int stepIndex = stepList.size();
+        StepComponent stepComponent = new StepComponent(this,
+                stepIndex,
+                Optional.ofNullable(step).map(Step::getStepDescriptions).orElse(null),
+                this::deleteStep);
+        stepList.add(stepComponent);
+        steps.addView(stepComponent, stepIndex);
+    }
+
+    private void deleteStep(int index) {
+        StepComponent stepComponent = (StepComponent) steps.getChildAt(index);
+        steps.removeView(stepComponent);
+        stepList.remove(stepComponent);
+        for (int i = 0; i < stepList.size(); i++) {
+            stepList.get(i).setIndex(i);
+        }
     }
 
     @Override
@@ -115,14 +160,27 @@ public class RecipeWriteActivity extends AppCompatActivity implements View.OnCli
                         .season((Season) season.getSelectedItem())
                         .difficulty((Difficulty) difficulty.getSelectedItem())
                         .price((Price) price.getSelectedItem())
+                        .steps(stepList.stream()
+                                .map(StepComponent::getDescription)
+                                .map(EditText::getText)
+                                .filter(Predicate.not(Objects::isNull))
+                                .map(Objects::toString)
+                                .filter(Predicate.not(String::isEmpty))
+                                .map(Step::new)
+                                .collect(Collectors.toList()))
                         .build();
                 DaoUtil.subscribe(recipeRepository.insert(recipe), this::finish, this);
             }
+        } else if (v == addStep) {
+            addStep();
         }
     }
 
     @Override
     public void afterTextChanged(Editable s) {
+        if (disableTextWatch) {
+            return;
+        }
         error.setVisibility(View.GONE);
         if (cleanName.getText().toString().isEmpty()) {
             cleanName.setError(getString(R.string.recipe_clean_name_error_empty));
---
+++ app/src/main/java/me/margotfrison/cookwriter/android/components/StepComponent.java
@@ -0,0 +1,51 @@
+package me.margotfrison.cookwriter.android.components;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import lombok.Getter;
+import me.margotfrison.cookwriter.R;
+
+@SuppressLint("ViewConstructor")
+@Getter
+public class StepComponent extends LinearLayout implements View.OnClickListener {
+    private int index;
+    private final TextView number;
+    private final EditText description;
+    private final ImageButton deleteStep;
+    private final StepListener listener;
+
+    public interface StepListener {
+        void onDelete(int index);
+    }
+
+    public StepComponent(Context context, int index, String stepDescription, StepListener listener) {
+        super(context);
+        inflate(context, R.layout.component_step, this);
+
+        this.listener = listener;
+
+        number = findViewById(R.id.number);
+        description = findViewById(R.id.description);
+        deleteStep = findViewById(R.id.delete_step);
+
+        setIndex(index);
+        description.setText(stepDescription);
+        deleteStep.setOnClickListener(this);
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+        number.setText(getResources().getString(R.string.recipe_step_number, index + 1));
+    }
+
+    @Override
+    public void onClick(View v) {
+        listener.onDelete(index);
+    }
+}
